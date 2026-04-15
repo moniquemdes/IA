2f0932a9f492c3f4037507ddf469a86cbe37445c
Monique Mendes <110635046+moniquemdes@users.noreply.github.com>
2026-04-15 12:03:08 -0300

    Update main.py

--- Trabalho 1 - Ant Clustering/main.py
+++ Trabalho 1 - Ant Clustering/main.py
@@ -1,49 +1,25 @@
 from ant import Ant
-from corpo import Corpo
 import random
 import math
-
-def carregar_corpos(caminho):
-    corpos = []
-
-    with open(caminho, 'r') as f:
-        for linha in f:
-            linha = linha.strip()
-
-            # ignora comentários e linhas vazias
-            if linha == "" or linha.startswith("#"):
-                continue
-
-            partes = linha.split()
-
-            x = float(partes[0].replace(",", "."))
-            y = float(partes[1].replace(",", "."))
-            grupo = int(partes[2])
-
-            corpos.append(Corpo(x, y, grupo))
-
-    return corpos
+import time  
 
 if __name__ == '__main__':
 
     TAMANHO_MATRIZ = 64
-    # NUM_CORPOS = 300
+    NUM_ITENS = 300
     NUM_FORMIGAS = 15
     ITERACOES = 100000
 
     matriz = [[0 for _ in range(TAMANHO_MATRIZ)] for _ in range(TAMANHO_MATRIZ)]
 
-   
-    corpos = carregar_corpos("base.txt")
+    itens_colocados = 0
+    while itens_colocados < NUM_ITENS:
+        x_rand = random.randint(0, TAMANHO_MATRIZ - 1)
+        y_rand = random.randint(0, TAMANHO_MATRIZ - 1)
 
-    for corpo in corpos:
-        while True:
-            x_rand = random.randint(0, TAMANHO_MATRIZ - 1)
-            y_rand = random.randint(0, TAMANHO_MATRIZ - 1)
-
-            if matriz[y_rand][x_rand] == 0:
-                matriz[y_rand][x_rand] = corpo
-                break
+        if matriz[y_rand][x_rand] == 0:
+            matriz[y_rand][x_rand] = 1
+            itens_colocados += 1
 
     formigas = []
     for i in range(NUM_FORMIGAS):
@@ -57,14 +33,15 @@ if __name__ == '__main__':
 
     print("\nEstado INICIAL da matriz:")
     for linha in matriz:
-        print(''.join(f"{x.grupo if x != 0 else ' ':>2}" for x in linha))
+        print(''.join(f"{'1' if x == 1 else ' ':>2}" for x in linha))
     print("-" * 40)
+    
+    tempo_inicio = time.time()
 
     for iteracao in range(ITERACOES):
         for formiga in formigas:
             densidade = formiga.calcular_densidade_local(matriz)
             direcao = random.choice(['up', 'down', 'left', 'right'])
-            
             
             if not formiga.ocupado: 
                 if matriz[formiga.y][formiga.x] == 1:
@@ -76,7 +53,6 @@ if __name__ == '__main__':
                 else: 
                     formiga.move(direcao, largura_matriz, altura_matriz)
 
-            
             else: 
                 if matriz[formiga.y][formiga.x] == 0:
                     if densidade > random.random(): 
@@ -87,11 +63,14 @@ if __name__ == '__main__':
                 else: 
                     formiga.move(direcao, largura_matriz, altura_matriz)
 
-    # --- PRINT MATRIZ FINAL ---
-    # print("\nEstado FINAL da matriz:")
-    # for linha in matriz:
-    #     print(''.join(f"{'1' if x == 1 else ' ':>2}" for x in linha))
-    # print("-" * 40)
 
-    #metrica de distancia vetorial / mediana / manhattan / cosseno / euclidiana 
-    #fazer com a vizinhança de cada formiga, calcular a distância média entre os corpos e comparar com a distância média inicial para avaliar a eficácia da organização.
+    tempo_fim = time.time()
+    
+    tempo_total = tempo_fim - tempo_inicio
+
+    print("\nEstado FINAL da matriz:")
+    for linha in matriz:
+        print(''.join(f"{'1' if x == 1 else ' ':>2}" for x in linha))
+    print("-" * 40)
+    
+    print(f"Tempo total de execução: {tempo_total:.2f} segundos.")
