ee0d9f571e54534c93f3e88eea00415123e05549
Monique Mendes <110635046+moniquemdes@users.noreply.github.com>
2026-04-18 18:05:51 -0300

    Update ant.py

--- Etapa 2/ant.py
+++ Etapa 2/ant.py
@@ -1,133 +1,72 @@
-from ant import Ant
-from corpo import Corpo
+import numpy as np
 import random
 import math
 
-def carregar_corpos(caminho):
-    corpos = []
+class Ant:
+    x = 0;
+    y = 0;
+    idAnt = 0;
 
-    with open(caminho, 'r') as f:
-        for linha in f:
-            linha = linha.strip()
 
-            if linha == "" or linha.startswith("#"):
-                continue
+    def __init__(self, x, y, idAnt, raio, ocupado=False):
+        self.x = x
+        self.y = y
+        self.idAnt = idAnt
+        self.raio = raio
+        self.ocupado = ocupado
+        self.item_carregado = None
 
-            partes = linha.split()
+#raio de visão 4-> cada formuiga pode ter um raio de visão das células adjacentes (cima, baixo, esquerda, direita)
 
-            x = float(partes[0].replace(",", "."))
-            y = float(partes[1].replace(",", "."))
-            grupo = int(partes[2])
+    def move(self, direcao, grid_width, grid_height):
+        if direcao == 'up' and self.y > 0:
+            self.y -= 1
+        elif direcao == 'down' and self.y < grid_height - 1:
+            self.y += 1
+        elif direcao == 'left' and self.x > 0:
+            self.x -= 1
+        elif direcao == 'right' and self.x < grid_width - 1:
+            self.x += 1
 
-            corpos.append(Corpo(x, y, grupo))
+    def calcular_similaridade(self, matriz, corpo_referencia):
+        if corpo_referencia is None:
+            return 0.0
 
-    return corpos
+        soma_similaridade = 0.0
+        total_celulas_vistas = 0
+       
+        alpha = 2
+        altura = len(matriz)
+        largura = len(matriz[0])
 
-if __name__ == '__main__':
+        for dy in range(-self.raio, self.raio + 1):
+            for dx in range(-self.raio, self.raio + 1):
+                
+                if dx == 0 and dy == 0:
+                    continue
 
-    TAMANHO_MATRIZ = 50
-    # NUM_CORPOS = 300
-    NUM_FORMIGAS = 150
-    ITERACOES = 700000000
+                x = (self.x + dx) % largura
+                y = (self.y + dy) % altura
 
-    matriz = [[0 for _ in range(TAMANHO_MATRIZ)] for _ in range(TAMANHO_MATRIZ)]
+                total_celulas_vistas += 1
+                vizinho = matriz[y][x]
 
-   
-    corpos = carregar_corpos("base.txt")
+                if vizinho != 0: 
+                    dist_euclidiana = math.sqrt((corpo_referencia.x - vizinho.x)**2 + (corpo_referencia.y - vizinho.y)**2)
 
-    for corpo in corpos:
-        while True:
-            x_rand = random.randint(0, TAMANHO_MATRIZ - 1)
-            y_rand = random.randint(0, TAMANHO_MATRIZ - 1)
+                    similaridade = 1.0 - (dist_euclidiana / alpha)
+                    soma_similaridade += similaridade
 
-            if matriz[y_rand][x_rand] == 0:
-                matriz[y_rand][x_rand] = corpo
-                break
+        if total_celulas_vistas == 0:
+            return 0.0
 
-    formigas = []
-    for i in range(NUM_FORMIGAS):
-        x_ant = random.randint(0, TAMANHO_MATRIZ - 1)
-        y_ant = random.randint(0, TAMANHO_MATRIZ - 1)
-        nova_formiga = Ant(x=x_ant, y=y_ant, idAnt=i+1, raio=1, ocupado=False)
-        formigas.append(nova_formiga)
+        f = soma_similaridade / total_celulas_vistas
+        return max(0.0, f)
 
-    altura_matriz = TAMANHO_MATRIZ
-    largura_matriz = TAMANHO_MATRIZ
-
-    print("\nEstado INICIAL da matriz:")
-    for linha in matriz:
-        print(''.join(f"{x.grupo if x != 0 else ' ':>2}" for x in linha))
-    print("-" * 40)
-
-    tempo_inicio = time.time()
-
-    for iteracao in range(ITERACOES):
-        for formiga in formigas:
-            
-            direcao = random.choice(['up', 'down', 'left', 'right'])
-            corpo_atual = matriz[formiga.y][formiga.x]
-            
-            
-            # if not formiga.ocupado: 
-            #     if matriz[formiga.y][formiga.x] == 1:
-            #         if probabilidade_pegar < random.random(): 
-            #             formiga.ocupado = True
-            #             matriz[formiga.y][formiga.x] = 0 
-            #         else:
-            #             formiga.move(direcao, largura_matriz, altura_matriz)
-            #     else: 
-            #         formiga.move(direcao, largura_matriz, altura_matriz)
-
-            
-            # else: 
-            #     if matriz[formiga.y][formiga.x] == 0:
-            #         if probabilidade_largar > random.random(): 
-            #             formiga.ocupado = False
-            #             matriz[formiga.y][formiga.x] = 1 
-            #         else:
-            #             formiga.move(direcao, largura_matriz, altura_matriz)
-            #     else: 
-            #         formiga.move(direcao, largura_matriz, altura_matriz)    
-
-            if not formiga.ocupado: 
-                if corpo_atual != 0:
-                    similaridade = formiga.calcular_similaridade(matriz, corpo_atual)
-                    prob_pegar = formiga.probabilidade_pegar(similaridade)
-                    
-                    if prob_pegar > random.random(): 
-                        formiga.ocupado = True
-                        formiga.item_carregado = corpo_atual 
-                        matriz[formiga.y][formiga.x] = 0     
-                    else:
-                        formiga.move(direcao, largura_matriz, altura_matriz)
-                else: 
-                    formiga.move(direcao, largura_matriz, altura_matriz)
-            
-            else: 
-                if corpo_atual == 0: 
-                   
-                    similaridade = formiga.calcular_similaridade(matriz, formiga.item_carregado)
-                    prob_largar = formiga.probabilidade_largar(similaridade)
-                    
-                    if prob_largar > random.random(): 
-                        matriz[formiga.y][formiga.x] = formiga.item_carregado 
-                        formiga.ocupado = False
-                        formiga.item_carregado = None
-                    else:
-                        formiga.move(direcao, largura_matriz, altura_matriz)
-                else: 
-                    formiga.move(direcao, largura_matriz, altura_matriz)
-
-    tempo_fim = time.time()
+    def probabilidade_pegar(self, similaridade):
+        k1 = 0.3 
+        return (k1 / (k1 + similaridade)) ** 2
     
-    tempo_total = tempo_fim - tempo_inicio
-
-    print("\nEstado FINAL da matriz:")
-    for linha in matriz:
-        print(''.join(f"{'1' if x == 1 else ' ':>2}" for x in linha))
-    print("-" * 40)
-    
-    print(f"Tempo total de execução: {tempo_total:.2f} segundos.")
-
-    #metrica de distancia vetorial / mediana / manhattan / cosseno / euclidiana 
-    #fazer com a vizinhança de cada formiga, calcular a distância média entre os corpos e comparar com a distância média inicial para avaliar a eficácia da organização.
+    def probabilidade_largar(self, similaridade):
+        k2 = 0.05
+        return (similaridade / (k2 + similaridade)) ** 2
